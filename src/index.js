a2528f363d056440c594ba44b861d8a0fc88ec36
Linderval-Moura <lindervalmatias@gmail.com>
2023-10-01 22:07:39 -0300

    Atualizado

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/');
+    cb(null, path.join(__dirname, '/uploads/'));
   },
   filename: function (req, file, cb) {
     const timestamp = new Date().toISOString().replace(/[-T:.Z]/g, '');
